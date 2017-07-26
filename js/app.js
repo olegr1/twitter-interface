b6eacdca091d8455a8e2039783cfb7bfb49d231d
Oleg Rybin <oleg.rybin@gmail.com>
2017-07-26 06:54:14 -0400

    WIP

--- js/app.js
+++ js/app.js
@@ -2,13 +2,18 @@ const express = require('express');
 const app = express();
 const path = require ('path');
 const config = require('./../config');
+const Twit = require('twit');
+
+console.log();
+
+const T = new Twit(config);
 
 app.use('/css', express.static(path.join(__dirname, './../css')));
 app.use('/images', express.static(path.join(__dirname, './../images')));
 
 console.log(path.join(__dirname, './../css'));
 
-app.set('views', path.join(__dirname, '/views'));
+app.set('views', path.join(__dirname, './../views'));
 app.set('view engine', 'pug');
 
 app.get('/', (req, res)=>{
