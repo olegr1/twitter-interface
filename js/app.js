03de5d5f707da3b81928926b97c40995c6bc9f63
Oleg Rybin <oleg.rybin@gmail.com>
2017-07-26 00:01:09 -0400

    Creating basic app structure

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const path = require ('path');
+const config = require('./../config');
+
+app.use('/css', express.static(path.join(__dirname, './../css')));
+app.use('/images', express.static(path.join(__dirname, './../images')));
+
+console.log(path.join(__dirname, './../css'));
+
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'pug');
+
+app.get('/', (req, res)=>{
+    res.render("index");
+});
+
+app.listen(3000, ()=>{
+    console.log("Listening on port 3000");
+});
+
+//console.log(config);
